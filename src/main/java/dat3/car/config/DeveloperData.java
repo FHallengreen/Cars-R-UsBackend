b69c157c8c0ce25e2521c26c8dd023e95a0aa91d
FHallengreen <fred70p9@stud.kea.dk>
2023-02-16 09:49:27 +0100

    Removed componentscan

--- src/main/java/dat3/car/config/DeveloperData.java
+++ src/main/java/dat3/car/config/DeveloperData.java
@@ -27,8 +27,6 @@ import java.util.List;
 import java.util.Map;
 
 @Configuration
-//@EnableJpaRepositories(basePackages = {"dat3.security.repository", "dat3.car.repository"})
-//@ComponentScan(basePackages = "dat3.security")
 public class DeveloperData implements ApplicationRunner {
 
     @Autowired
